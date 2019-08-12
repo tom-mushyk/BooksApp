582ce1dff91a1f9e95ffb9025ace29ca707d3d3e
Tomasz <nevalive@gmail.com>
2019-08-12 09:29:17 +0200

    added json pretty support

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'rjkrj29r023jr02'
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 db.init_app(app)
 migrate.init_app(app, db)
